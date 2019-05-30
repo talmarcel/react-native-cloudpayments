37597e84a6b92344e8cbde492f5becf9983406a6
Dmitry Sytsevich <zavylon@MacBook-Pro-Dmitry.local>
2019-05-30 16:48:20 +0300

    SDWebViewController

---
+++ ios/SDWebViewController/SDWebViewController.h
@@ -0,0 +1,18 @@
+//
+//  SDWebViewController.h
+//  SDWebViewController
+//
+//  Created by Dmitry Sytsevich on 5/30/19.
+//  Copyright © 2019 Dmitry Sytsevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol SDWebViewDelegate;
+@interface SDWebViewController : UIViewController
+
+@property (weak, nonatomic) id<SDWebViewDelegate> m_delegate;
+
+- (id)initWithURL:(id)url login:(NSString *)login password:(NSString *)password;
+
+@end
