6c4380ddc9891e8782259118a5980c06948f2a39
Mikhail Yarmoluk <hellboymxb@gmail.com>
2019-05-30 18:09:08 +0300

    Code cleanup

--- android/src/main/java/com/rncloudpayments/CheckoutActivity.java
+++ android/src/main/java/com/rncloudpayments/CheckoutActivity.java
@@ -2,12 +2,9 @@ package com.rncloudpayments;
 
 import android.app.Activity;
 import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.view.Window;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.WritableMap;
